2229f3919b907361442c8bc38e65b17a9fc8fd8a
moldingtofu <jeremykchang@gmail.com>
2019-09-17 20:42:11 -0400

    added delete and select functionality

--- lab2/scripts.js
+++ lab2/scripts.js
@@ -1,5 +1,17 @@
+/* array containing all tasks */
 var tasks = [];
+/* number of selected tasks to determine whether button is red or black */
+var numSelected = 0;
 
+/* constructor for a task */
+function Task(title, text) {
+    this.id = tasks.length;
+    this.title = title;
+    this.text = text;
+    this.selected = false;
+}
+
+/* handles creation of a task card */
 let submit = document.getElementById('submitButton');
 submit.addEventListener('click', () => {
     let title = document.getElementById('titleInput').value;
@@ -8,6 +20,7 @@ submit.addEventListener('click', () => {
     updateTasks(tasks);
 });
 
+/* handles search queries and returns card with title that contains query */
 let search = document.getElementById('searchBar');
 search.addEventListener('input', e => {
     let query = search.value.trim()
@@ -20,27 +33,47 @@ search.addEventListener('input', e => {
     }
 });
 
-if (tasks.length) {
-    let selected = document.getElementById('task');
-    selected.addEventListener('click', () => {
-        let task = selected.value;
-        console.log(task);
-    });
-}
+/* deletes selected tasks */
+let deleteButton = document.getElementById('deleteButton');
+deleteButton.addEventListener('click', () => {
+    tasks = tasks.filter(task => !task.selected);
+    updateTasks(tasks);
+})
 
-function Task(title, text) {
-    this.title = title;
-    this.text = text;
-}
-
+/* update the task list with a new card and add event listener */
 function updateTasks(list) {
     clearTasks();
     let taskBar = document.getElementById('notecardList');
     list.forEach( task => {
         taskBar.appendChild(createCard(task));
     });
+
+    let selected = taskBar.childNodes;
+    selected.forEach( card => {
+        card.addEventListener('click', () => {
+            if (card.className == 'card') {
+                card.className = 'card text-white bg-danger';
+                tasks.filter(task => task.id == card.id).map(task => task.selected = true);
+                numSelected += 1;
+            }
+            else {
+                card.className = 'card';
+                tasks.filter(task => task.id == card.id).map(task => task.selected = false);
+                numSelected -= 1;
+            }
+
+            let button = document.getElementById('deleteButton');
+            if (numSelected == 0) {
+                button.className = 'btn btn-outline-dark';
+            }
+            else {
+                button.className = 'btn btn-outline-danger';
+            }
+        });
+    });
 }
 
+/* reset task list to nothing (super efficient) */
 function clearTasks() {
     let taskBar = document.getElementById('notecardList');
     while (taskBar.firstChild) {
@@ -48,6 +81,7 @@ function clearTasks() {
     }
 }
 
+/* create a card to add to task list */
 function createCard(task) {
     let taskBar = document.getElementById('notecardList');
     let newTask = document.createElement('div');
@@ -59,7 +93,7 @@ function createCard(task) {
     newTask.className = 'card';
     newTask.appendChild(title);
     newTask.appendChild(text);
-    newTask.id = 'task';
+    newTask.id = task.id;
 
     return newTask;
 }
