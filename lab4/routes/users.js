5f80cedc536ee89ab0a85bc6d97b7b1e0612b18e
moldingtofu <jeremykchang@gmail.com>
2019-11-01 23:33:24 -0400

    lab4

--- lab4/routes/users.js
+++ lab4/routes/users.js
@@ -4,26 +4,73 @@ const User = require('../models/user')
 
 router.get('/', async (req, res) => {
   // TODO: return all users
+  User.find((err, docs) => {
+    if (err) {
+      console.log(err)
+      res.status(404).send('Not found')
+    }
+    else {
+      res.send(docs)
+    }
+  });
 })
 
 router.post('/', async (req, res) => {
   // TODO: create a user
+  let newUser = new User(req.body);
+  newUser.save((err, product) => {
+    if (err) {
+      console.log(err)
+      res.status(404).send('Not found')
+    }
+    else {
+      res.send(product)
+    }
+  });
 })
 
-router.get('/:id', getUser, (req, res) => {
+router.get('/:id', async (req, res) => {
   // TODO: return a single user by id
+  let id = req.params.id
+  User.findById(id, (err, docs) => {
+    if (err) {
+      console.log(err)
+      res.status(404).send('Not found')
+    }
+    else {
+      res.send(docs)
+    }
+  });
 })
 
-router.put('/:id', getUser, async (req, res) => {
+router.put('/:id', async (req, res) => {
   // TODO:update a user
+  console.log('put')
+  let id = req.params['id']
+  User.findByIdAndUpdate(id, req.body, (err) => {
+    if (err, docs) {
+      console.log(err)
+      res.status(404).send('Not found')
+    }
+    else {
+      res.send(docs)
+    }
+  });
 })
 
-router.delete('/:id', getUser, async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // TODO:delete a user
+  console.log('delete')
+  let id = req.params['id'];
+  User.findByIdAndDelete(id, (err, docs) => {
+    if (err) {
+      console.log(err)
+      res.status(404).send('Not found')
+    }
+    else {
+      res.send(docs)
+    }
+  });
 })
-
-async function getUser(req, res, next) {
-  // Not required, but could be used to get users with a specific id or all users
-}
 
 module.exports = router 
